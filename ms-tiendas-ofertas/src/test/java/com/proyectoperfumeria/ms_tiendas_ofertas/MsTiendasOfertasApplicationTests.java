6d12f0719e61e36b22af331bd198ee80e60e9ba0
PETAKA <fe.astudillon@duocuc.cl>
2026-05-07 21:52:53 -0400

    Agregando estructura base de carpetas de los microservicios

---
+++ ms-tiendas-ofertas/src/test/java/com/proyectoperfumeria/ms_tiendas_ofertas/MsTiendasOfertasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoperfumeria.ms_tiendas_ofertas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTiendasOfertasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
