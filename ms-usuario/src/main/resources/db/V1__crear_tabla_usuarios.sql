53521475bd7a68db22e15ddf5624341d51fb8ef4
mauro <mau.santiago@duocuc.cl>
2026-05-09 23:13:24 -0400

    borre v1

--- ms-usuario/src/main/resources/db/V1__crear_tabla_usuarios.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE usuarios (
-                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          nombre_completo VARCHAR(150) NOT NULL,
-                          email VARCHAR(100) NOT NULL UNIQUE,
-                          password VARCHAR(100) NOT NULL,
-                          rol VARCHAR(50) NOT NULL
-);
