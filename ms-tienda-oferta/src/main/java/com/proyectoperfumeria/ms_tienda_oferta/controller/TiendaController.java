a889f0865921276c38544d8427974782cba46db1
PETAKA <fe.astudillon@duocuc.cl>
2026-05-11 19:22:17 -0400

    ms-tienda-oferta dado por finalizado

---
+++ ms-tienda-oferta/src/main/java/com/proyectoperfumeria/ms_tienda_oferta/controller/TiendaController.java
@@ -0,0 +1,44 @@
+package com.proyectoperfumeria.ms_tienda_oferta.controller;
+
+import com.proyectoperfumeria.ms_tienda_oferta.dto.*;
+import com.proyectoperfumeria.ms_tienda_oferta.service.TiendaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class TiendaController {
+
+    private final TiendaService tiendaService;
+
+    @PostMapping("/tiendas")
+    public ResponseEntity<?> crearTienda(@RequestBody @Valid TiendaRequestDTO tiendaRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tiendaService.crearTienda(tiendaRequestDTO));
+    }
+
+    @GetMapping("/tiendas")
+    public ResponseEntity<List<TiendaResponseDTO>> listarTiendas() {
+        return ResponseEntity.ok(tiendaService.obtenerTiendas());
+    }
+
+    @PostMapping("/ofertas")
+    public ResponseEntity<OfertaResponseDTO> crearOferta(@RequestBody @Valid OfertaRequestDTO ofertaRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tiendaService.crearOferta(ofertaRequestDTO));
+    }
+
+    @GetMapping("/ofertas")
+    public ResponseEntity<List<OfertaResponseDTO>> listarOfertas() {
+        return ResponseEntity.ok(tiendaService.obtenerOfertasPorTienda());
+    }
+
+    @PostMapping("/ventas")
+    public ResponseEntity<VentaResponseDTO> registrarVenta(@RequestBody @Valid VentaRequestDTO ventaRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tiendaService.registrarVentaPerfume(ventaRequestDTO));
+    }
+}
---
+++ ms-tienda-oferta/src/main/java/com/proyectoperfumeria/ms_tienda_oferta/service/TiendaService.java
@@ -0,0 +1,95 @@
+package com.proyectoperfumeria.ms_tienda_oferta.service;
+
+
+import com.proyectoperfumeria.ms_tienda_oferta.dto.*;
+import com.proyectoperfumeria.ms_tienda_oferta.model.Oferta;
+import com.proyectoperfumeria.ms_tienda_oferta.model.Tienda;
+import com.proyectoperfumeria.ms_tienda_oferta.model.Venta;
+import com.proyectoperfumeria.ms_tienda_oferta.repository.OfertaRepository;
+import com.proyectoperfumeria.ms_tienda_oferta.repository.TiendaRepository;
+import com.proyectoperfumeria.ms_tienda_oferta.repository.VentaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TiendaService {
+
+    private final VentaRepository ventaRepository;
+    private final OfertaRepository ofertaRepository;
+    private final TiendaRepository tiendaRepository;
+
+    public TiendaResponseDTO mapToTiendaDTO(Tienda tienda) {
+           return new TiendaResponseDTO(tienda.getId(), tienda.getNombreTienda(), tienda.getUbicacion());
+    }
+
+    public TiendaResponseDTO crearTienda(TiendaRequestDTO tiendaRequestDTO) {
+        Tienda tienda = new Tienda();
+        tienda.setNombreTienda(tiendaRequestDTO.getNombre());
+        tienda.setUbicacion(tiendaRequestDTO.getUbicacion());
+        return mapToTiendaDTO(tiendaRepository.save(tienda));
+    }
+
+    public List<TiendaResponseDTO> obtenerTiendas(){
+        List<Tienda> tiendas = tiendaRepository.findAll();
+        List<TiendaResponseDTO> tiendasDTO = new ArrayList<>();
+        for (Tienda tienda : tiendas) {
+            tiendasDTO.add(mapToTiendaDTO(tienda));
+        }
+        return tiendasDTO;
+    }
+
+    private OfertaResponseDTO mapToOfertaDTO(Oferta oferta) {
+        return new OfertaResponseDTO(
+                oferta.getId(),
+                oferta.getPerfumeId(),
+                oferta.getTiendaId(),
+                oferta.getPrecioOferta());
+        }
+
+    public OfertaResponseDTO crearOferta (OfertaRequestDTO ofertaRequestDTO) {
+        Oferta oferta = new Oferta();
+        oferta.setPerfumeId(ofertaRequestDTO.getPerfumeId());
+        oferta.setTiendaId(ofertaRequestDTO.getTiendaId());
+        oferta.setPrecioOferta(ofertaRequestDTO.getPrecioOferta());
+        return mapToOfertaDTO(ofertaRepository.save(oferta));
+    }
+
+    public List<OfertaResponseDTO> obtenerOfertasPorTienda() {
+        List<Oferta> ofertas = ofertaRepository.findAll();
+        List<OfertaResponseDTO> ofertasDTO = new ArrayList<>();
+        for (Oferta oferta : ofertas) {
+            ofertasDTO.add(mapToOfertaDTO(oferta));
+        }
+        return ofertasDTO;
+    }
+
+    private VentaResponseDTO mapToVentaDTO(Venta venta) {
+        return new VentaResponseDTO(
+                venta.getId(),
+                venta.getUsuarioId(),
+                venta.getPerfumeId(),
+                venta.getFechaVenta(),
+                venta.getTotal(),
+                venta.getMetodoPago(),
+                venta.getEstadoPago()
+        );
+    }
+
+    public VentaResponseDTO registrarVentaPerfume(VentaRequestDTO ventaRequestDTO) {
+        Venta venta = new Venta();
+        venta.setUsuarioId(ventaRequestDTO.getUsuarioId());
+        venta.setPerfumeId(ventaRequestDTO.getPerfumeId());
+        venta.setTotal(ventaRequestDTO.getTotal());
+        venta.setMetodoPago(ventaRequestDTO.getMetodoPago());
+        venta.setFechaVenta(LocalDateTime.now());
+        venta.setEstadoPago("PAGO APROBADO!");
+        Venta ventaGuardada = ventaRepository.save(venta);
+        return mapToVentaDTO(ventaGuardada);
+    }
+
+}
