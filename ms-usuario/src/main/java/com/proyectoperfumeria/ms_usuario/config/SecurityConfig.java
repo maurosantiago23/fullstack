40a8f1af632497f49ef09a4076854341dcae1149
mauro <mau.santiago@duocuc.cl>
2026-05-15 15:48:55 -0400

    securityconfig en config

---
+++ ms-usuario/src/main/java/com/proyectoperfumeria/ms_usuario/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.proyectoperfumeria.ms_usuario.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/usuarios/registro", "/api/v1/usuarios/login").permitAll().anyRequest().permitAll());
+        return http.build();
+    }
+}
