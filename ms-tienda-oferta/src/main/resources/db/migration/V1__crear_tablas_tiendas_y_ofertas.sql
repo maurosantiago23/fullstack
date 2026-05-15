13cf178313702da906ee3e5081788ba2db44aa56
mauro <mau.santiago@duocuc.cl>
2026-05-15 17:43:55 -0400

    corregido el V1 tienda oferta y agregada tabla ventas

--- ms-tienda-oferta/src/main/resources/db/migration/V1__crear_tablas_tiendas_y_ofertas.sql
+++ ms-tienda-oferta/src/main/resources/db/migration/V1__crear_tablas_tiendas_y_ofertas.sql
@@ -10,9 +10,19 @@ CREATE TABLE ofertas (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_producto BIGINT NOT NULL,
     id_tienda BIGINT NOT NULL,
-    precio_normal DECIMAL(10, 2) NOT NULL,
-    precio_oferta DECIMAL(10, 2),
+    precio_normal INT NOT NULL,
+    precio_oferta INT,
     stock_disponible INT DEFAULT 0,
     fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_tienda FOREIGN KEY (id_tienda) REFERENCES tiendas(id)
+);
+
+CREATE TABLE ventas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario BIGINT NOT NULL,
+    id_perfume BIGINT NOT NULL,
+    fecha_venta DATETIME(6),
+    total INT NOT NULL,
+    metodo_pago VARCHAR(50),
+    estado_pago VARCHAR(50)
 );
