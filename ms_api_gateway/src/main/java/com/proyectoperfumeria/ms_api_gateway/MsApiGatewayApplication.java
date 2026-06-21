653c532db7d204e945a1520666194dd1875d0d55
mauro <mau.santiago@duocuc.cl>
2026-06-20 23:30:56 -0400

    Arreglado xd puse mal la carpeta

---
+++ ms_api_gateway/src/main/java/com/proyectoperfumeria/ms_api_gateway/MsApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.proyectoperfumeria.ms_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayApplication.class, args);
+	}
+
+}
--- ms_eureka_server/src/main/java/com/proyectoperfumeria/ms_eureka_server/MsEurekaServerApplication.java
+++ ms_eureka_server/src/main/java/com/proyectoperfumeria/ms_eureka_server/MsEurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.proyectoperfumeria.ms_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
