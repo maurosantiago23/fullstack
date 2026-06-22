8d3541935ca66f1b374209eb1c6fd8545e433447
mauro <mau.santiago@duocuc.cl>
2026-06-22 18:56:56 -0400

    url configurable de feignclient de ms-tiendaoferta

--- ms-tienda-oferta/src/main/java/com/proyectoperfumeria/ms_tienda_oferta/client/UsuarioFeignClient.java
+++ ms-tienda-oferta/src/main/java/com/proyectoperfumeria/ms_tienda_oferta/client/UsuarioFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-usuarios", url = "http://localhost:8081") //corregido https a http -_-
+@FeignClient(name = "ms-usuarios", url = "${clients.ms-usuario.url:http://localhost:8081}") //corregido a una url configurable
 public interface UsuarioFeignClient {
 
     @GetMapping("/api/v1/usuarios/{id}")
