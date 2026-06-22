c1642d46904036fa59593232f27b7e5946edb897
mauro <mau.santiago@duocuc.cl>
2026-06-22 19:02:39 -0400

    url configurable de feignclientcatalogo de ms-analisis

--- ms-analisis-experiencia/src/main/java/com/proyectoperfumeria/ms_analisis_experiencia/client/CatalogoFeignClient.java
+++ ms-analisis-experiencia/src/main/java/com/proyectoperfumeria/ms_analisis_experiencia/client/CatalogoFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-catalogo", url = "http://localhost:8082")
+@FeignClient(name = "ms-catalogo", url = "${clients.ms-catalogo.url:http://localhost:8082}") //corregido a una url configurable
 public interface CatalogoFeignClient {
 
     @GetMapping("/api/v1/perfumes/{id}")
